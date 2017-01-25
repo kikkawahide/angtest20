bca2a0959f182eba2cb8fba218ed9ae82248ffbf
kikkawahide <kikkawa9512@gmail.com>
2017-01-25 15:49:35 +0900

    add1

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app sample!';
+
+demoData: any = [
+    {name: '山田', age: 24},
+    {name: '田中', age: 28},
+    {name: '佐藤', age: 18},
+    {name: '井上', age: 32},
+    {name: '高橋', age: 46}
+
+]
+
+}  
